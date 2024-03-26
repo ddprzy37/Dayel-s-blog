9aa8664bff434303ad952d03d3c5e94e985519ae
ddprzy37 <122937215+ddprzy37@users.noreply.github.com>
2024-03-26 14:38:00 -0600

    added basic JS elements

---
+++ Assets/jss/script.js
@@ -0,0 +1,20 @@
+// References to DOM elements
+const blogForm = document.getElementById('blogform');
+const blogPostsContainer = document.getElementById('blogPosts');
+const modeToggleBtn = document.getElementById('modeToggle');
+
+// Event listener
+blogForm.addEventListener('submit', function(event){
+    event.preventDefault();
+});
+
+// Render blog posts
+function renderBlogPosts() {
+
+}
+// Event listener for toggle mode
+modeToggleBtn.addEventListener('click', function() {
+
+});
+
+renderBlogPosts();
